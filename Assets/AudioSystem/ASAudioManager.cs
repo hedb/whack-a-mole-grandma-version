151588adc343bb6c197548abf5b79e7a9f913bf6
hedb <hed.barnissan@gmail.com>
2020-08-07 12:55:09 +0300

    initial-version

---
+++ Assets/AudioSystem/ASAudioManager.cs
@@ -0,0 +1,130 @@
+//-----------------------------------------------------------------
+// Central Audio Class - Present in all scenes.
+//-----------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+namespace AudioSystem
+{
+	public class ASAudioManager : MonoBehaviour
+	{
+
+		#region Singleton pattern (Awake)
+
+		private static ASAudioManager _ins;
+		public static ASAudioManager ins
+		{
+			get
+			{
+				if (_ins == null)
+				{
+					_ins = GameObject.FindObjectOfType<ASAudioManager>();
+
+					DontDestroyOnLoad(_ins.gameObject);
+				}
+
+				return _ins;
+			}
+			set
+			{
+				_ins = value;
+			}
+		}
+
+		void Awake()
+		{
+			if (_ins == null)
+			{
+				// Populate with first instance
+				_ins = this;
+				DontDestroyOnLoad(this);
+			}
+			else
+			{
+				// Another instance exists, destroy
+				if (this != _ins)
+					Destroy(this.gameObject);
+			}
+		}
+
+		#endregion
+
+		[SerializeField]
+		private GameObject audioPool;	// Gameobject that hosts the AudioSources for pool sounds
+
+		[SerializeField]
+		private AudioMixerGroup masterMixer;
+
+		[SerializeField]
+		private ASSound[] pool;			// A pool with a bunch of sounds and settings for said sounds
+
+		void Start()
+		{
+			if (audioPool == null)
+			{
+				Debug.LogError("ASAudioManager: No audioPool object referenced.");
+				return;
+			}
+
+			// Cycle through sounds and add sources
+			for (int i = 0; i < pool.Length; i++)
+			{
+				pool[i].source = audioPool.AddComponent<AudioSource>();
+			}
+		}
+
+
+		private ASSound find_by_name(string _soundName)
+		{
+
+			ASSound ret = Array.Find(pool,
+				element => element.name == _soundName);
+
+			if (ret == null)
+			{
+				//Debug.LogWarning("Sound: '" + _soundName + "' couldn't be played because it wasn't found.");
+			}
+
+			return ret;
+		}
+
+		public void stop(string _soundName)
+        {
+
+			ASSound _sound = find_by_name(_soundName);
+			if (_sound != null)
+			{
+				_sound.source.Stop();
+			}
+
+		}
+
+		// Play a sound from the pool
+		public void Play(string _soundName)
+		{
+
+			ASSound _sound = find_by_name(_soundName);
+			AudioSource _source = _sound.source;
+			_source.clip = _sound.sound;
+			_source.volume = UnityEngine.Random.Range(_sound.baseVolume - _sound.volumeVariance, _sound.baseVolume + _sound.volumeVariance);
+			_source.pitch = UnityEngine.Random.Range(_sound.basePitch - _sound.pitchVariance, _sound.basePitch + _sound.pitchVariance);
+
+			_source.spatialBlend = 0;
+
+			if (_sound.mixer == null)
+			{
+				_source.outputAudioMixerGroup = masterMixer;
+			}
+			else
+			{
+				_source.outputAudioMixerGroup = _sound.mixer;
+			}
+
+			_source.Play();
+		}
+
+	}
+
+}
---
+++ Assets/Grandchild.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioSystem;
+
+public class Grandchild : MonoBehaviour
+{
+    private float previous_change = 0;
+    private string chosen_clip = "";
+
+    private ASAudioManager audioManager;
+
+    enum State
+    {
+        Showing,
+        Caught,
+        Kissed,
+        Hiding
+    }
+    State current_state = State.Hiding;
+
+
+    private string[] audioclips = new string[] { "hed", "avsha", };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioManager = ASAudioManager.ins;
+        if (audioManager == null) Debug.LogError("No ASAudioManager?!");
+
+        hide();
+    }
+
+
+    void hide()
+    {
+        this.transform.position = new Vector3(-100, -100, 0);
+        previous_change = Time.fixedTime;
+        current_state = State.Hiding;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (current_state == State.Caught) {
+            current_state = State.Kissed;
+            audioManager.stop(chosen_clip);
+            audioManager.Play("kiss");
+            previous_change = Time.fixedTime;
+        }
+        else if (current_state == State.Kissed && Time.fixedTime-previous_change > 2)
+        {
+            hide();
+            current_state = State.Hiding;
+            audioManager.stop("kiss");
+            previous_change = Time.fixedTime;
+
+        }
+        else if (current_state == State.Hiding && Time.fixedTime - previous_change > 4)
+        {
+            current_state = State.Showing;
+            previous_change = Time.fixedTime;
+
+            int x = Random.Range(-5, 5);
+            int y = Random.Range(-5, 5);
+            this.transform.position = new Vector3(x, y, 0);
+
+            chosen_clip = audioclips[Random.Range(0, audioclips.Length)];
+            audioManager.Play(chosen_clip);
+
+        }
+    }
+
+
+
+    void OnMouseDown()
+    {
+        current_state = State.Caught;
+    }
+
+}
